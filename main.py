a020a28d7e4d3b6f9fa9aa94406c3f4af6fbbee2
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2023-09-22 19:38:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ dim=int(input("user enter dimensions"))
 ttt = TicTacToe(dim)
 ttt.print_board()
 print(ttt.empty())
+
+for i in range(9):
+    ttt.turns()
+    ttt.inputboard()
+    ttt.print_board()
+    if ttt.checkwin()==True:
+        break
+    
+
