d7891cf387282a1d51aa1003dc87014fe05623d5
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2023-09-22 19:37:59 +0530

    Update algo.py

--- algo.py
+++ algo.py
@@ -7,25 +7,26 @@ class TicTacToe:
             for j in range(n):
                 ele.append(" ")
             self.board.append(ele)
+        self.turn=0
+        self.player=0
         
     def print_board(self):
-        print(self.board)
         #n is dimensions
         #self.board is the data matrix
         
         for i in range(self.n):
-            for j in range(self.n-1):
-                print(self.board[i][j],"|",end=" ")
-            print()
+            for j in range(self.n):
+                if j!=self.n-1:
+                    print(self.board[i][j],"|",end=" ")
+                else:
+                    print(self.board[i][j])
+            
             if i==self.n-1:
                 pass
             else:
                 for i in range(self.n):
                     print("_"," ",end=" ")
-                print()
-        
-            
-        
+                print() 
     
     def empty(self):
         lst=[]
@@ -34,3 +35,48 @@ class TicTacToe:
                 if self.board[i][j]==" ":
                     lst.append([i,j])
         return lst
+
+    def turns(self):
+        if self.turn%2==0:
+            self.player=1
+            self.marker="X"
+            print("it is player1's turn")
+        else:
+            self.player=2 
+            self.marker="O"
+            print("it is player2's turn")
+        self.turn+=1
+        
+                
+    def inputboard(self):
+        print("user enter row and colum to deploy move")
+        #space seperated integer input
+        x,y=map(int,input().split())
+        self.board[x][y]=self.marker
+
+    def checkwin(self):
+        self.win=False
+
+        #horizontal wins
+        for i in range(self.n):
+            #horizontal wins
+            if self.board[i][0]==self.board[i][1] and self.board[i][0]==self.board[i][2] and self.board[i][0]!=" " :
+                self.win=True 
+
+            #vertical wins
+            if self.board[0][i]==self.board[1][i] and self.board[0][i]==self.board[2][i] and self.board[i][0]!=" " :
+                self.win=True 
+            
+        #diagonal wins
+        if self.board[0][0]==self.board[1][1] and self.board[0][0]==self.board[2][2]and self.board[0][0]!=" " :
+            self.win=True
+        if self.board[0][2]==self.board[1][1] and self.board[0][2]==self.board[2][0]and self.board[2][0]!=" " :
+            self.win=True 
+        
+        if self.win==True:
+            print("game over")
+
+        else:
+            pass
+
+        return self.win
